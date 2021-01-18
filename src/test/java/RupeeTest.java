5849cafb87e452499a978e0abc3a2bc5ae2edd9d
Sudhira <sudhira.17it@kct.ac.in>
2021-01-18 11:40:35 +0530

    [Gaurav/Sudhira] : Add. Implementation to check the equality of two rupees

--- src/test/java/RupeeTest.java
+++ src/test/java/RupeeTest.java
@@ -1,6 +1,10 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+
 public class RupeeTest {
     @Test
     void comparingObjectWithItself() {
@@ -27,5 +31,21 @@ public class RupeeTest {
         boolean value = rupee1.compareRupee(rupee2);
 
         Assertions.assertFalse(value);
+    }
+
+    @Test
+    void shouldReturnTrueForTwoEqualValuedRupees() {
+        Rupee rupee1 = new Rupee(10,123);
+        Rupee rupee2 = new Rupee(10,123);
+
+        assertEquals(rupee1.value,rupee2.value);
+    }
+
+    @Test
+    void shouldReturnFalseForTwoDifferentValuedRupees() {
+        Rupee rupee1 = new Rupee(10,123);
+        Rupee rupee2 = new Rupee(12,123);
+
+        assertNotEquals(rupee1.value,rupee2.value);
     }
 }
